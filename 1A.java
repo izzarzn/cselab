d9ceeab2ad1458c599dbbe8e549f26db9917d323
Arjun Hegde <arjunmeega28@gmail.com>
2022-09-03 07:42:03 +0530

    Update 1A.java

--- 1A.java
+++ 1A.java
@@ -1,1 +1,22 @@
+import java.util.*;
 
+class Student{
+
+String USN,Name,Branch,Phone;
+Scanner scan = new Scanner(System.in);
+
+void read(){
+
+  System.out.printf("Enter student Details");
+  System.out.printf("Enter USN");
+  USN = scan.next();
+  System.out.printf("Enter Branch");
+  Name = scan.next();
+  System.out.printf("Enter Name");
+  Branch = scan.next();
+  System.out.printf("Enter Phone");
+  Phone = scan.next();
+
+public static void main( String[] args)
+{
+  
