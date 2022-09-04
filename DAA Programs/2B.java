db4c0c425dd2a8863e2f218c5fc5546905906889
Arjun Hegde <arjunmeega28@gmail.com>
2022-09-04 20:13:39 +0530

    Update 2B.java

--- DAA Programs/2B.java
+++ DAA Programs/2B.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+class Customer {
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    String name;
+    System.out.println("Enter the details in format <DD/MM/YYYY>:);
+    name = scan.next();
+    StringTokenizer st = new StringTokenizer(name, ",/");                   
+    int count = countTokens();
+    for(int i=1;i<= count && hasMoreTokens();i++)
+        {
+          System.out.print(nextToken());
+           if(i<count)
+             {
+                System.out.print(",");
+              }
+       }
+                       }
+              }  
