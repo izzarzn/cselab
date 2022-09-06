aa17965665040cc755fcb5aad83d1bebbcfa1a97
Arjun Hegde <arjunmeega28@gmail.com>
2022-09-06 21:35:54 +0530

    Update 8.Krushkals.java

--- DAA Programs/8.Krushkals.java
+++ DAA Programs/8.Krushkals.java
@@ -1,38 +1,51 @@
-static void Krushkals()
-{
- int a=b=u=v=0;
- int i,j,min;
- int mincost=0;
- int ne=1;
- while(ne<n){
-  for(i=1,min=999;i<=n;i++){
-     for(j=1;j<=n;j++){
-     min=cost[i][j];
-     a=u=i;
-     b=v=j;
-     }
-   }
- }  
-  u=find(u);
-  v=find(v);
-  if(u!=v)
-  {
-  uni(a,v
-  SOP (" ne (a,b) is min);
-  mincost=mincost+min;
-  }
-  cost[a][b]=cost[b][a]=999;
-  }
-  SOP("Mincost");
-  static int find(int i){
-  while(parent[i]!=0;
-  i=parent[i];
-  return i;
-  }
-  static int uni(int i,int j)
-  {
-  parent[j]=i;
-  }
-  }
-  
-  
+import java.util.Scanner;
+public class Kruscalunion {
+	static int [] parent =new int [50]; 
+	static int [][]cost = new int [10][10]; 
+	static int a,b,i,j,u,v,n,min,no=1,mincost=0;
+	static int find(int w)
+	{
+	while(parent[w]!=0)
+	w=parent[w];
+	return w;
+	}
+	static void union()
+	{
+	if(u!=v)
+	{
+	no++;
+	System.out.println(no-1 + "Edge("+a+","+b+")="+min);
+	mincost+=min;
+	parent[v]=u;
+	}
+	cost[a][b]=cost[b][a]=999;
+	}
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in); 
+		System.out.println("Enter the no. of vertices:"); 
+		n=read.nextInt();
+		System.out.println("Enter the cost adjacency matrix, 999 for no direct path:");
+		for(i=1;i<=n;i++)
+		for(j=1;j<=n;j++)
+		cost[i][j]=read.nextInt();
+		while(no<n)
+		{
+		min=999;
+		for(i=1; i<=n ; i++)
+		{
+		for(j=1; j<=n; j++)
+		{
+		if(cost[i][j] < min)
+		{
+		min=cost[i][j];
+		a=u=i;
+		b=v=j;
+		} } }
+		u=find(u);
+		v=find(v);
+		union();
+		}
+		System.out.println("minimum cost ="+mincost);
+		} }
+
+
