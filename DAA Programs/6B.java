2cb3efc47138a6f98e53936552c8be300558ca71
Arjun Hegde <arjunmeega28@gmail.com>
2022-09-06 21:34:27 +0530

    Update 6B.java

--- DAA Programs/6B.java
+++ DAA Programs/6B.java
@@ -1,62 +1,76 @@
-import java.util.Scanner; class KObject {
-float w; float p; float r;
+import java.util.Scanner;
+class Greedy
+{
+	float w;
+	float p;
+	float r;
 }
-public class KnapsackGreedy {
-static final int MAX = 20; static int n;
-static float M;
-// Knapsack object details
-// max. no. of objects // no. of objects
-// capacity of Knapsack
-public static void main(String args[]) {
-Scanner scanner = new Scanner(System.in); 
-  System.out.println("Enter number of objects: ");
-  n = scanner.nextInt();
-KObject[] obj = new KObject[n];
-for(int i = 0; i<n;i++)
-obj[i] = new KObject();// allocate memory for members
-ReadObjects(obj);
-  Knapsack(obj); 
-  scanner.close();
-}
-static void ReadObjects(KObject obj[]) {
-KObject temp = new KObject();
-Scanner scanner = new Scanner(System.in);
-  System.out.println("Enter the max capacity of knapsack: "); 
-  M = scanner.nextFloat();
-System.out.println("Enter Weights: "); 
-  for (int i = 0; i < n; i++)
-obj[i].w = scanner.nextFloat();
-System.out.println("Enter Profits: ");
-  for (int i = 0; i < n; i++)
-obj[i].p = scanner.nextFloat();
-for (int i = 0; i < n; i++)
-obj[i].r = obj[i].p / obj[i].w;
-// sort objects in descending order, based on p/w ratio for(int i = 0; i<n-1; i++)
-for(int j=0; j<n-1-i; j++) 
-  if(obj[j].r < obj[j+1].r){
-temp = obj[j]; obj[j] = obj[j+1];
-obj[j+1] = temp; }
-scanner.close(); }
-static void Knapsack(KObject kobj[]) { 
-  float x[] = new float[MAX];
-float totalprofit;
-int i;
-float U; // U place holder for M U = M;
-totalprofit = 0;
-for (i = 0; i < n; i++)
-x[i] = 0;
-for (i = 0; i < n; i++) {
-if (kobj[i].w > U) break;
-else {
-x[i] = 1;
-totalprofit = totalprofit + kobj[i].p;
-U = U - kobj[i].w; }
-}
-System.out.println("i = " + i); if (i < n)
-x[i] = U / kobj[i].w;
-totalprofit = totalprofit + (x[i] * kobj[i].p); 
-  System.out.println("The Solution vector, x[]: "); for (i = 0; i < n; i++)
-System.out.print(x[i] + " "); 
-  System.out.println("\nTotal profit is = " + totalprofit);
-  }
-}
+public class Kobject {
+	static final int max = 20;
+	static int n; static int i; static int r;
+	static float m;
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number of objects");
+		n = input.nextInt();
+		Greedy[]obj = new Greedy[n];
+		for(i=0; i<n; i++)
+			obj[i] = new Greedy();
+		ReadObject (obj);
+		knapsack(obj);
+	}
+	static void ReadObject(Greedy obj[])
+	{
+		Greedy temp = new Greedy();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the maximum capacity of KnapSack");
+		m = input.nextFloat();
+		System.out.println("Enter weight");
+		for(i=0; i<n; i++) {
+			obj[i].w = input.nextFloat();
+	}
+		System.out.println("Enter Profit");
+		for(i=0; i<n; i++) {
+			obj[i].p = input.nextFloat();
+		}
+		for(i=0; i<n; i++) {
+			obj[i].r = obj[i].p / obj[i].w;
+	}
+	for(i=0; i<n-1; i++) {
+		for(int j=0; j<n-1-i; j++) {
+			if(obj[i].r < obj[j+1].r) {
+				temp = obj[j];
+	            obj[j] = obj[j+1];
+	            obj[j+1] = temp;
+		} } } }
+  static void knapsack(Greedy obj[])
+{ 
+	float x[] = new float[max];
+	float totalprofit;
+	int i; 
+	float rc;
+	rc = m;
+	totalprofit = 0;
+	for(i=0; i<n; i++){
+		 x[i] = 0;
+	for(i=0; i<n; i++)
+	{
+		if(obj[i].w > rc) {
+			break;
+		}
+		else
+		{
+			x[i] = 1;
+			totalprofit = totalprofit + obj[i].p;
+			rc = rc - obj[i].w;
+		} }
+	System.out.println("i" + i);
+	if(i<=n)
+		x[i] = rc/obj[i].w;
+	totalprofit = totalprofit + obj[i].p * x[i];
+	System.out.println("The Solution vector , x[]");
+	for(i=0; i<n; i++)
+		System.out.println(x[i] + " ");
+	System.out.println("\n TotalProfit = " + totalprofit);
+} } }
+
